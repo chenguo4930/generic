ed299cd9646b23fc11da1f74bad8f210ade617ef
chengguo <guo.cheng3@zhenai.com>
2018-10-08 19:01:24 +0800

    修复了例子

--- app/src/main/java/com/generic/lib/generic/test5.kt
+++ app/src/main/java/com/generic/lib/generic/test5.kt
@@ -1,10 +1,24 @@
 package com.generic.lib.generic
 
 /**
+ * 子类型 ： 任何时候如果需要的是类型 A 的值，你的偶能够使用类型 B 的值，当作 A 的值，类型 B 就称为类型 A 的子类型
+ *            比如：Int 是 Number的子类型，但 Int 不是 String 的子类型，Int 也是 Int 的子类型，
+ *            任何类型都可以被认为是它自己的子类型
+ *
+ * 超类型：子类型的反义词，如果 A 是 B 的子类型，那么 B 就是 A 的超类型。
+ *
+ *          非空类型 A 是可空类型 A? 的子类型
+ *
+ *
+ *
+ *
+ *
  * @author ChengGuo
  * @date 2018/9/28
  */
 fun main() {
+    //
+
 
 }
 
