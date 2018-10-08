cbb2066e584115ff4f3bd5c5b5724bba9699fccf
cheng <chengguo4930@hotmail.com>
2018-10-08 23:20:06 +0800

    泛型示例

--- app/src/main/java/com/generic/lib/generic/test5.kt
+++ app/src/main/java/com/generic/lib/generic/test5.kt
@@ -16,9 +16,75 @@ package com.generic.lib.generic
  * @author ChengGuo
  * @date 2018/9/28
  */
-fun main() {
-    //
+
+fun main(args: Array<String>) {
+//    val a: Int? = 10000
+//    val b: Int = 10000
+//
+//    println("a= =b ${a == b}")
+//    println("a= = = ${a === b}")
 
 
 }
 
+
+/**
+ * out : 声明为协变，只读  两层含义 1、子类型化会被保留  2、T 只能在out位置  例如：List 接口
+ * in : 声明为协变，只写
+ * Function1
+ *
+ */
+interface Producer<in T, out R> {
+    //    fun produce(t: T): T
+    fun produce(t: T): R
+}
+
+open class Animal {
+    fun feed() {
+    }
+}
+
+/**
+ * T没有任何变型修饰符的情况
+ */
+//class Herd<T : Animal> {
+class Herd<out T : Animal> {
+    val size: Int get() = 5
+    operator fun get(i: Int): T {
+        return get(0)
+    }
+}
+
+fun feedAll(animals: Herd<Animal>) {
+    for (i in 0 until animals.size) {
+        animals[i].feed()
+    }
+}
+
+class Cat : Animal() {
+    fun cleanLitter() {
+    }
+}
+
+fun takeCareOfCats(cats: Herd<Cat>) {
+    for (i in 0 until cats.size) {
+        cats[i].cleanLitter()
+    }
+    //        feedAll(cats)
+}
+
+/**
+ * out 的其他注意事项
+ */
+interface List<out T> : Collection<T> {
+    fun subList(fromIndex: Int, toIndex: Int): List<T>  //这里的T也在Out位置上
+}
+
+/**
+ * 构造方法的参数既不在in位置，也不在out的位置，即使声明为了out，也可以在构造方法中使用
+ * 因为构造方法不是那种在实例创建完成之后还能调用的方法，没有潜在的危险
+ * 私有方法 private 的参数即不在 in 位置和 out 位置
+ */
+class Herd2<out T : Animal>(vararg animals: T) {
+
+}
